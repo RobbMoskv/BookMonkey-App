78cd18c1a4299b3a6cf36b635da4f034c52623fd
Fabrizio <Fabrizio_39@hotmail.com>
2019-01-01 18:56:45 +0100

    Added a new component for list items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bm-root',
-  templateUrl: './app.component.html',
+  template: '<bm-book-list></bm-book-list>',
   styles: []
 })
-export class AppComponent {
-  title = 'BookMonkey';
-}
+export class AppComponent {}
---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Book, Thumbnail } from '../shared/book';
+
+@Component({
+  selector: 'bm-book-list',
+  templateUrl: './book-list.component.html',
+  styles: []
+})
+export class BookListComponent implements OnInit {
+  books: Book[];
+  constructor() {}
+  // S. 94
+  ngOnInit() {
+    this.books = [
+      new Book(
+        '923842378',
+        'Mathe mit James',
+        ['James Brown', 'Harald Shore'],
+        new Date(2018, 12, 19),
+        'Grundlagen der Mathematik Vol 2',
+        5,
+        [
+          new Thumbnail(
+            'https://ng-buch.de/cover2.jpg',
+            'Hilfe in der Mathematik'
+          )
+        ]
+      ),
+      new Book(
+        '923842019',
+        'Sprache leicht gelernt',
+        ['JMartina Meile', 'Kati Hering', 'Thomas Magular'],
+        new Date(2014, 4, 2),
+        'Lernen SIe auf einfach weise die Sprachen der Welt.',
+        2,
+        [
+          new Thumbnail(
+            'https://ng-buch.de/cover1.jpg',
+            'Lernen kann auch Spass machen.'
+          )
+        ]
+      )
+    ];
+  }
+}
