48cbc8eaba1619404f072f63971ef6cd91c50320
Fabrizio <Fabrizio_39@hotmail.com>
2019-01-10 17:28:07 +0100

    Finished the search component part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 // Components
 import { BookListComponent } from './book-list/book-list.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
+
+import { debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'bm-search',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  // Target
+  @Output() bookSelected = new EventEmitter<Book>();
 
+  keyup = new EventEmitter<string>();
+  foundBooks: Book[] = [];
+  isLoading = false;
+
+  constructor(private bs: BookStoreService) { }
+
+  // The benefit by using switchMap() instead of mergeMap() / flatMap() is, that pending network requests are consolidated
+  // in case new observables have been received.
   ngOnInit() {
+    this.keyup
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        tap(() => this.isLoading = true),
+        switchMap(searchTerm => this.bs.getAllbySearch(searchTerm)),
+        tap(() => this.isLoading = false),
+      )
+      .subscribe(books => this.foundBooks = books);
   }
 
 }
