c376471de4f087e46a3b4deba403e63b9a92f2b7
Fabrizio <Fabrizio_39@hotmail.com>
2019-01-29 16:35:33 +0100

    Implemented custom pipe for ISBN

--- src/app/shared/isbn.pipe.ts
+++ src/app/shared/isbn.pipe.ts
@@ -5,8 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IsbnPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any, addPrefix: boolean): string {
+    // Filter value that is not valid
+    if (!value || value.length !== 10 && value.length !== 13) {
+      return null;
+    }
+
+    // Decide which prefix should be used
+    let prefix = '';
+    if (addPrefix) {
+      prefix = (value.length === 10) ? 'ISBN-10: ' : 'ISBN-13: ';
+    }
+
+    // Return ISBN 10 OR ISBN 13 string
+    if (value.length === 10) {
+      return prefix + value;
+    } else {
+      return `${prefix}${value.substr(0, 3)}-${value.substr(3)}`;
+    }
   }
 
 }
