dcb2be3440177ad6d82fa00bd40d5123009e498a
Fabrizio <Fabrizio_39@hotmail.com>
2019-02-03 22:20:08 +0100

    Zoom directive implemented

--- src/app/shared/zoom.directive.ts
+++ src/app/shared/zoom.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[bmZoom]'
 })
 export class ZoomDirective {
+  // Access the Host element
+  @HostBinding('class.small') isZoomed: boolean;
 
-  constructor() { }
+  // Host listener when focus gets on the element
+  @HostListener('mouseenter') onMouseEnter() {
+    this.isZoomed = true;
+  }
+
+  // Host listener when focus gets off the element
+  @HostListener('mouseleave') onmouseleave() {
+    this.isZoomed = false;
+  }
 
 }
