eccc31580588e0becb2f6537ceb1582bb91e100c
Fabrizio <Fabrizio_39@hotmail.com>
2019-02-06 21:06:58 +0100

    Implemented a CanActivate guard for the admin route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { CanNavigateToAdminGuard } from './can-navigate-to-admin.guard';
 
 // Components
 import { HomeComponent } from './home/home.component';
@@ -7,9 +8,17 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+
     // Lazy loading routes
-    { path: 'books', loadChildren: './book/book.module#BookModule' },
-    { path: 'admin', loadChildren: './admin/admin.module#AdminModule' }
+    {
+        path: 'books',
+        loadChildren: './book/book.module#BookModule'
+    },
+    {
+        path: 'admin',
+        loadChildren: './admin/admin.module#AdminModule',
+        canActivate: [CanNavigateToAdminGuard] // Guarded
+    }
 ];
 
 @NgModule({
@@ -17,6 +26,6 @@ const routes: Routes = [
     // Also pass the preloading strategy
     imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
     exports: [RouterModule],
-    providers: []
+    providers: [CanNavigateToAdminGuard]
 })
 export class AppRoutingModule { }
--- src/app/can-navigate-to-admin.guard.ts
+++ src/app/can-navigate-to-admin.guard.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CanNavigateToAdminGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+
+  accessGranted = false;
+
+  canActivate(): boolean {
+    // Make sure window gets triggered the first time
+    if (!this.accessGranted) {
+      this.accessGranted = window.confirm('Möchten Sie den Admin-Bereich wirklich betreten?');
+    }
+    return this.accessGranted;
   }
 }
