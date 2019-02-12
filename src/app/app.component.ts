2a699d115ea2e2f32b68b06d0330fb4ed7cc8358
Fabrizio <Fabrizio_39@hotmail.com>
2019-02-12 21:56:35 +0100

    Working on search to make it available on each site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Book } from './shared/book';
 
 @Component({
   selector: 'bm-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+
+  bookSelected(book: Book) {
+    this.router.navigate(['../books', book.isbn], { relativeTo: this.route });
+  }
+}
